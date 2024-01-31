db30d9059453dcac49f702a399258265c90cf830
Julius Welzel <julius.welzel@gmail.com>
2024-01-31 13:50:30 +0100

    [ADD] impedance check

--- eeg_sanity/check_sanity.py
+++ eeg_sanity/check_sanity.py
@@ -4,8 +4,13 @@ import mne
 import numpy as np
 from pathlib import Path
 
-dir_data = Path(r'C:\Users\User\Desktop\kiel\lab\lab-utilities\eeg_sanity') # update the path to your folder
-fname_bv = Path.joinpath(dir_data,"raw","RO26BU.vhdr") # adjust the filename for your filo oi
+dir_data = Path(r'W:\source_projects\senseAge_ls\pilot_data') # update the path to your folder
+# find the latest file with the extension .vhdr
+fname_bv = dir_data.glob('*.vhdr')
+fname_bv = max(fname_bv, key=lambda p: p.stat().st_ctime)
+fname_bv = str(fname_bv)
+
+
 
 # load data
 raw = mne.io.read_raw_brainvision(fname_bv, verbose=False, preload=True);
@@ -24,7 +29,7 @@ ed_data = mne.preprocessing.compute_bridged_electrodes(raw)
 bridged_idx, ed_matrix = ed_data
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
-fig.suptitle('Aguf07 Electrical Distance Matrix')
+fig.suptitle('Electrical Distance Matrix')
 
 # take median across epochs, only use upper triangular, lower is NaNs
 ed_plot = np.zeros(ed_matrix.shape[1:]) * np.nan
@@ -50,22 +55,53 @@ fig.show()
 
 mne.viz.plot_bridged_electrodes(
     raw.info, bridged_idx, ed_matrix,
-    title='Aguf07 Bridged Electrodes', topomap_args=dict(vlim=(None, 5)));
+    title='Bridged Electrodes', topomap_args=dict(vlim=(None, 5)));
 
-# get impedances, all but ground
-all_imp = [info["imp"] for i, info in raw.impedances.items() if i != 'Gnd']
 
-rng = np.random.default_rng(11)  # seed for reproducibility
+# find all vdhr files
+fnames_bv_all = dir_data.glob('*.vhdr')
 
-# typically impedances < 25 kOhm are acceptable for active systems and
-# impedances < 5 kOhm are desirable for a passive system
-impedances = all_imp
+# loop over all vdhr files and save the plots for the impedance
+for fname_bv in fnames_bv_all:
+    fname_bv = str(fname_bv)
+    raw = mne.io.read_raw_brainvision(fname_bv, verbose=False, preload=True);
+    # check if bva got the correct channel locations
+    chns_layout = raw.get_montage()
+    
+    # get impedancesfrom eeg channels
+    ch_types = raw.ch_names()
+    eeg_chs = mne.pick_types(raw.info, meg=False, eeg=True)
+    all_imp = [info["imp"] for i, info in raw.impedances.items() if i != "ECG" or i != "33"]
 
-cmap = LinearSegmentedColormap.from_list(
-    name="impedance_cmap", colors=["g", "y", "r"], N=256
-)
-fig, ax = plt.subplots(figsize=(5, 5))
-im, cn = mne.viz.plot_topomap(impedances, raw.info, axes=ax, cmap=cmap, vlim=(0, 30))
-ax.set_title("Electrode Impendances")
-cax = fig.colorbar(im, ax=ax)
-cax.set_label(r"Impedance (k$\Omega$)")
+    # typically impedances < 25 kOhm are acceptable for active systems and
+    # impedances < 5 kOhm are desirable for a passive system
+    impedances = [imp for imp in all_imp if not np.isnan(imp)]
+
+    cmap = LinearSegmentedColormap.from_list(
+        name="impedance_cmap", colors=["g", "y", "r"], N=len(impedances) - 2
+    )
+    fig, ax = plt.subplots(figsize=(5, 5))
+    im, cn = mne.viz.plot_topomap(impedances, raw.info, axes=ax, cmap=cmap, vlim=(0, 50))
+    ax.set_title("Electrode Impendances")
+    cax = fig.colorbar(im, ax=ax)
+    cax.set_label(r"Impedance (k$\Omega$)")
+
+    # find the electrode with the hightest impedance and mark it in the tpopplot with the value
+    max_imp = max(impedances)
+    max_imp_idx = impedances.index(max_imp)
+    ax.plot(
+        raw.info["chs"][max_imp_idx]["loc"][0],
+        raw.info["chs"][max_imp_idx]["loc"][1],
+        "r.",
+        markersize=20,
+        label=f"Max Impedance: {max_imp:.2f} kOhm",
+    )
+
+    # add legend
+    ax.legend()
+    # save the plot
+    fname = fname_bv.split('.')[0]
+    fname = dir_data.joinpath(fname + '_impedances.png')
+    fig.savefig(fname, dpi=300)
+
+
